d555ba7ed5f59bfd88cb0149d6200b368671185f
Ridvan <ridvanm9@yahoo.com>
2022-05-23 14:15:04 +0200

    date picker

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>jQuery Calendar picker</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+</head>
+<body>
+	Pic a date:
+	<input id="date" type="text" name="datepicker" size="8" />
+
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/jquery-ui.js"></script>
+	<script type="text/javascript" src="js/ui.js"></script>
+</body>
+</html>
---
+++ js/ui.js
@@ -0,0 +1,2 @@
+$('#date').datepicker({ dateFormat: 'dd/mm/yy', minDate: '-5d', maxDate: '+1m +2d',
+						showButtonPanel: true, showAnim: 'show' });
